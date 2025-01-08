1aeac9cdbde58dff36add56a20ead929f9a46bcc
ayush <ayushmangiri@gmail.com>
2025-01-08 21:25:08 +0530

    08-01-2025

---
+++ bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.bookStore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookController {
+    
+	 @GetMapping ("/")
+	 public String home () {
+		 return "home";
+	 }
+	 @GetMapping("/book_register")
+	 public String bookRegister() {
+	 return "bookRegister";
+	 }
+	 @GetMapping("/available_book")
+	 public String getAllBook() {
+		 return "bookList";
+		 
+	 }
+}
